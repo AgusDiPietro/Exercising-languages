5b466404d120b3ccd1c0c61c9f216080c66c85b4
Agus <dipietro.agustin97@gmail.com>
2024-11-20 19:09:42 +0100

    using SyncRWMutex

--- Go/Practicing/Chanel/SyncMutex/main.go
+++ Go/Practicing/Chanel/SyncMutex/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 type Counter struct {
-	mu    sync.Mutex //lo usamos para bloquear acceso simultaneo a los recursos compartidos
+	mu    sync.RWMutex //lo usamos para bloquear acceso simultaneo a los recursos compartidos
 	value int
 }
 
 func (c *Counter) Increment() {
 	c.mu.Lock()
-	defer c.mu.Unlock() //bloqueamos y desbloqueamos el metodo dentro de la go rutine
+	defer c.mu.Unlock() //bloqueamos y desbloqueamos la go rutine
 	c.value++
 }
 
 func (c *Counter) Value() int {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.mu.RLock() //bloqueo solo para la lectura
+	defer c.mu.RUnlock()
 	return c.value
 }
 
@@ -31,6 +31,14 @@ func main() {
 			defer wg.Done()
 			c.Increment()
 		}()
+	}
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("Actual value:", c.Value())
+		}()
+
 	}
 	wg.Wait()
 	fmt.Println("Final Counter:", c.Value())
