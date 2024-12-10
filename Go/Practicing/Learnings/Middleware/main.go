5a9b1abbe47ae374091ae04ec796a482853a5990
Agus <dipietro.agustin97@gmail.com>
2024-12-10 14:05:12 +0100

    using a basic middleware

---
+++ Go/Practicing/Learnings/Middleware/main.go
@@ -0,0 +1,26 @@
+// funcion que se ejecuta antes o despues que un controlador maneje una solicitud, permite utilizarla
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		fmt.Printf("Request %s %s %s \n", r.Method, r.RequestURI, time.Since(start))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome to homescreen")
+}
+
+func main() {
+	http.Handle("/", loggingMiddleware(http.HandleFunc(homeHandler)))
+	fmt.Println("Server executing in http://localhost:8000")
+	http.ListenAndServe(":8000", nil)
+}
