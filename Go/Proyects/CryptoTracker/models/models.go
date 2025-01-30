f7e056cd851d93a30e2a803c1dad80bb12506611
Agus <dipietro.agustin97@gmail.com>
2025-01-30 14:42:48 +0100

    adding a form to save transactions

--- Go/Proyects/CryptoTracker/models/models.go
+++ Go/Proyects/CryptoTracker/models/models.go
@@ -1,9 +1,21 @@
 package models
 
+import (
+	"time"
+)
+
 type CoinData struct {
 	ID           string  `json:"id"`
 	Name         string  `json:"name"`
 	Symbol       string  `json:"symbol"`
 	CurrentPrice float64 `json:"current_price"`
 	MarketCap    float64 `json:"market_cap"`
+}
+
+type Transaction struct {
+	Type        string    `json:"type"` // "buy" o "sell"
+	CoinSymbol  string    `json:"coin_symbol"`
+	USDAmount   float64   `json:"usd_amount"`
+	TokenAmount float64   `json:"token_amount"`
+	Timestamp   time.Time `json:"timestamp"`
 }
